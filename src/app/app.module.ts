bc14173eaf4afe43e45a5a88c802c286ee9c658a
Gurusanthosh-P <gurusp@brimmatech.com>
2024-01-05 20:26:11 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { loader } from './services/loader/loader';
+import { MessageService } from 'primeng/api';
+
+import { SharedModule } from './shared_modules/shared.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
-  providers: [],
+  providers: [loader,MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
